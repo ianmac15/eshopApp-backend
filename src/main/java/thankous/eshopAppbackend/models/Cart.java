3f0cb924aac5c207b5b5c1c7cf8554f9605abed3
ianmac15 <3thita@gmail.com>
2022-06-07 19:59:35 +0300

    tried postgres

--- src/main/java/thankous/eshopAppbackend/models/Cart.java
+++ src/main/java/thankous/eshopAppbackend/models/Cart.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Table(name = "cart")
 public class Cart {
 
     @Id
--- src/main/java/thankous/eshopAppbackend/models/Product.java
+++ src/main/java/thankous/eshopAppbackend/models/Product.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Table(name = "product")
 public class Product {
 
     @Id
