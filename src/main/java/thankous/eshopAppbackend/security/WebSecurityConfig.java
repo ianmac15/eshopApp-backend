d09b5f3a8c1ff4e7d0253e634bd10a8bc29314a5
ianmac15 <44259297+ianmac15@users.noreply.github.com>
2022-06-17 14:55:18 +0300

    1

---
+++ src/main/java/thankous/eshopAppbackend/security/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package thankous.eshopAppbackend.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity
+public class WebSecurityConfig {
+}
